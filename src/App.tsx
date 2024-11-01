cb8f0228e91af8289ebd1ecb207263a8105c24ea
Quốc Dũng <nguyenquocdung26032003@gmail.com>
2024-11-01 14:37:11 +0700

    feat/create-product-cart-component (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,18 @@ import LayoutAdmin from "./Layout/AdminLayout/LayoutAdmin"
 import { AdminRoute } from "./Routes/adminRoute"
 
 function App() {
-
   return (
-  <Routes>
-    <Route path="admin" element={<LayoutAdmin/>}>
-          {
-            AdminRoute.map((route,index)=>{
-              return (
-                <Route key={index+0} path={route.path} element={<route.element/>}/>
-              )
-            })
-          }
-        </Route>
-  </Routes>
+    <Routes>
+      <Route path="admin" element={<LayoutAdmin />}>
+        {
+          AdminRoute.map((route, index) => {
+            return (
+              <Route key={index + 0} path={route.path} element={<route.element />} />
+            )
+          })
+        }
+      </Route>
+    </Routes>
   )
 }
 
