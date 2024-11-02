96e3c8465c81d7a4979fe39c96b499d83c332022
Dũng Nguyễn Quốc <nguyenquocdung26032003@gmail.com>
2024-11-02 15:45:14 +0700

    feat/test2

---
+++ src/Routes/AdminRoute.tsx
@@ -0,0 +1,14 @@
+import { routerLinkAdmin } from "../Util/routerLink"
+import HomePageAdmin from "../Page/AdminPage/HomePage/HomePageAdmin"
+import UserManagement from "../Page/AdminPage/UserPage/UserManagement"
+
+export const AdminRoute = [
+  {
+    path: routerLinkAdmin.Home,
+    element: HomePageAdmin,
+  },
+  {
+    path: routerLinkAdmin.User,
+    element: UserManagement,
+  },
+]
