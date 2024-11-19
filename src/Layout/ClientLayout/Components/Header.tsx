cf0010d6456e0b116cb2d52a71b01e6cf6213676
cbtruong <batruong12122002@gmail.com>
2024-11-19 08:20:53 +0700

    update assetList

--- src/Layout/ClientLayout/Components/Header.tsx
+++ src/Layout/ClientLayout/Components/Header.tsx
@@ -2,26 +2,76 @@ import Logo from "../../../Assets/Image/User/Logo.png";
 import Avatar from "../../../Assets/Image/User/Avatar.png";
 
 import { GoBell } from "react-icons/go";
+import { IoChevronForwardOutline } from "react-icons/io5";
 
 const Header = () => {
 	return (
 		<>
-			<header className="h-[100px] bg-primary px-[6rem] flex justify-between">
+			<header className="h-[100px] bg-primary px-custom flex justify-between">
 				{/* left menu */}
 				<div className="flex  justify-center gap-8 items-center">
 					<div className="flex items-center flex-col cursor-pointer">
 						<img src={Logo} alt="" className="w-14 h-14" />
 						<p className="text-white">Auction Table</p>
 					</div>
-					<div className="flex gap-8">
-						<p className="text-white cursor-pointer">Home</p>
-						<p className="text-white cursor-pointer">Catergory</p>
-						<p className="cursor-pointer">Sell</p>
+					<div className="flex gap-10">
+						<div className="group flex gap-1 items-center relative cursor-pointer">
+							<p className="text-white text-sx font-medium">
+								Tài sản đấu giá
+							</p>
+							<IoChevronForwardOutline className="rotate-[90deg] text-white text-lg group-hover:rotate-[270deg] transitionLow" />
+							<div
+								className="absolute top-16 min-w-44 bg-white w-40 py-3 opacity-0 -translate-y-4
+  shadow-custom group-hover:translate-y-0 group-hover:opacity-100 transitionLow"
+							>
+								<div className="py-2 cursor-pointer transitionLow group/SelectMenu">
+									<p className="px-3 font-semibold group-hover/SelectMenu:text-primary  transitionLow">
+										Tài sản nhà nước
+									</p>
+									<div className="h-1 w-0 group-hover/SelectMenu:w-full border-b-2 transitionLow origin-left border-primary"></div>
+								</div>
+
+								<div className="py-2 cursor-pointer transitionLow group/SelectMenu">
+									<p className="px-3 font-semibold group-hover/SelectMenu:text-primary  transitionLow">
+										Bất động sản
+									</p>
+									<div className="h-1 w-0 group-hover/SelectMenu:w-full border-b-2 transitionLow origin-left border-primary"></div>
+								</div>
+							</div>
+						</div>
+						<div className="group flex gap-1 items-center relative cursor-pointer">
+							<p className="text-white text-sx font-medium">
+								Cuộc đấu giá
+							</p>
+							<IoChevronForwardOutline className="rotate-[90deg] text-white text-lg group-hover:rotate-[270deg] transitionLow" />
+							<div
+								className="absolute top-16 min-w-44 bg-white w-40 py-3 opacity-0 -translate-y-4
+  shadow-custom group-hover:translate-y-0 group-hover:opacity-100 transitionLow"
+							>
+								<div className="py-2 cursor-pointer transitionLow group/SelectMenu">
+									<p className="px-3 font-semibold group-hover/SelectMenu:text-primary  transitionLow">
+										Tài sản nhà nước
+									</p>
+									<div className="h-1 w-0 group-hover/SelectMenu:w-full border-b-2 transitionLow origin-left border-primary"></div>
+								</div>
+
+								<div className="py-2 cursor-pointer transitionLow group/SelectMenu">
+									<p className="px-3 font-semibold group-hover/SelectMenu:text-primary  transitionLow">
+										Bất động sản
+									</p>
+									<div className="h-1 w-0 group-hover/SelectMenu:w-full border-b-2 transitionLow origin-left border-primary"></div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 				{/* right menu */}
 				<div className="flex items-center justify-center">
 					<div className="flex items-center gap-8">
+						<div className="flex flex-col ">
+							<p className="text-white text-2xl font-medium">03:32:19</p>
+							<p className="text-white text-sm">Thứ Ba, 19/11/2024</p>
+						</div>
 						<GoBell className="text-[1.5rem] cursor-pointer" />
 						<div className="w-14 h-14 rounded-full overflow-hidden">
 							<img src={Avatar} alt="" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,34 @@
-import Icons from "vite-plugin-icons";
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 export default {
+	mode: 'jit',
+	purge: ['./src/**/*.{html,js,ts,tsx}'],
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
+			spacing: {
+				"custom":"6rem",
+			  },
+			boxShadow: {
+				'custom': '0px 7px 29px 0px rgba(100, 100, 111, 0.2)',
+			  },
 			colors: {
 				primary: "#FF4949",
 			},
+			
 		},
 	},
-	plugins: [Icons()],
+	plugins: [
+		plugin(function ({ addUtilities }) {
+			addUtilities({
+			  '.transitionLow': {
+				transition: 'all 0.8s ease-in-out',
+			  },
+			  '.transitionHight': {
+				transition: 'all 0.3s ease-in-out',
+			  },
+			});
+		  }),
+	],
 };
